68aeb2503451185842b38e11d3f814c8dfe358e6
Junio Santos <junio-gon@hotmail.com>
2019-10-29 13:14:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
+<?php require_once 'config.php'; ?>
+<?php require_once DBAPI; ?>
+
+<?php include(HEADER_TEMPLATE); ?>
+<?php $db = open_database(); ?>
+
 <h1>Dashboard</h1>
 <hr />
 
+<?php if ($db) : ?>
+
 <div class="row">
 	<div class="col-xs-6 col-sm-3 col-md-2">
-		<a href="customers/add.php" class="btn btn-primary">
+		<a href="clientes/add.php" class="btn btn-primary">
 			<div class="row">
 				<div class="col-xs-12 text-center">
 					<i class="fa fa-plus fa-5x"></i>
@@ -16,7 +24,7 @@
 	</div>
 
 	<div class="col-xs-6 col-sm-3 col-md-2">
-		<a href="customers" class="btn btn-default">
+		<a href="clientes" class="btn btn-default">
 			<div class="row">
 				<div class="col-xs-12 text-center">
 					<i class="fa fa-user fa-5x"></i>
@@ -29,6 +37,11 @@
 	</div>
 </div>
 
+<?php else : ?>
 	<div class="alert alert-danger" role="alert">
 		<p><strong>ERRO:</strong> Não foi possível Conectar ao Banco de Dados!</p>
 	</div>
+
+<?php endif; ?>
+
+<?php include(FOOTER_TEMPLATE); ?>
